79a60d122dbb70afc8e4d73f25b0c5f1229da7fe
Ivan Hrytsai <ivan.hrytsai@magefan.com>
2024-05-13 13:08:43 +0300

    Add Info About Plan

---
+++ Block/Adminhtml/System/Config/Form/InfoConversionApi.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Copyright © Magefan (support@magefan.com). All rights reserved.
+ * Please visit Magefan.com for license details (https://magefan.com/end-user-license-agreement).
+ */
+
+declare(strict_types=1);
+
+namespace Magefan\FacebookPixel\Block\Adminhtml\System\Config\Form;
+
+class InfoConversionApi extends InfoPlan
+{
+
+    /**
+     * @return string
+     */
+    protected function getMinPlan(): string
+    {
+        return 'Extra';
+    }
+
+    /**
+     * @return string
+     */
+    protected function getSectionId(): string
+    {
+        return 'mffacebookpixel_conversion_api';
+    }
+
+    /**
+     * @return string
+     */
+    protected function getText(): string
+    {
+        return 'Conversion Api options are available in <strong>Extra</strong> plans only.';
+    }
+}
